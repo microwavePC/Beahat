9a8ac41e69f2814b6a71a140635b017105663408
microwavePC <amagai.tohoku@gmail.com>
2017-05-13 18:20:45 +0900

    Android4.4に対応

--- Beahat/Plugin.Beahat.Android/BeahatImplementation.cs
+++ Beahat/Plugin.Beahat.Android/BeahatImplementation.cs
@@ -8,247 +8,397 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
+using Android.OS;
 
 [assembly: Dependency(typeof(BeahatImplementation))]
 namespace Plugin.Beahat
 {
-    /// <summary>
-    /// Implementation for Feature
-    /// </summary>
-    public class BeahatImplementation : BindableBase, IBeahat
-    {
+	/// <summary>
+	/// Implementation for Feature
+	/// </summary>
+	public class BeahatImplementation : BindableBase, IBeahat
+	{
 
-        #region PRERTIES
+		#region PRERTIES
 
-        private bool _isScanning = false;
-        public bool IsScanning
-        {
-            get { return _isScanning; }
-            set { SetProperty(ref _isScanning, value); }
-        }
+		private bool _isScanning = false;
+		public bool IsScanning
+		{
+			get { return _isScanning; }
+			set { SetProperty(ref _isScanning, value); }
+		}
 
-        public List<iBeacon> DetectedBeaconList
-        {
-            get { return new List<iBeacon>(_scanCallback.DetectedBeaconDict.Values); }
-        }
+		public List<iBeacon> DetectedBeaconList
+		{
+			get
+			{
+				if (OS_VER < BuildVersionCodes.Lollipop)
+				{
+					return new List<iBeacon>(_scanCallbackOld.DetectedBeaconDict.Values);
+				}
+				else
+				{
+					return new List<iBeacon>(_scanCallbackNew.DetectedBeaconDict.Values);
+				}
+			}
+		}
 
-        #endregion
-
-        
-        #region FIELDS
-
-        private BluetoothManager _btManager;
-        private BluetoothAdapter _btAdapter;
-        private BluetoothLeScanner _bleScanner;
-        private BleScanCallback _scanCallback;
-
-        #endregion
-        
-
-        #region CONSTRUCTOR
-
-        public BeahatImplementation()
-        {
-            _btManager = (BluetoothManager)Android.App.Application.Context.GetSystemService("bluetooth");
-            _btAdapter = _btManager.Adapter;
-            _scanCallback = new BleScanCallback();
-            
-            try
-            {
-                _bleScanner = _btAdapter.BluetoothLeScanner;
-            }
-            catch (Exception ex)
-            {
-
-            }
-        }
-
-        #endregion
+		#endregion
 
 
-        #region PUBLIC METHODS
+		#region FIELDS
 
-        public bool BluetoothIsAvailableOnThisDevice()
-        {
-            return !(_btAdapter == null);
-        }
+		private BluetoothManager _btManager;
+		private BluetoothAdapter _btAdapter;
+		private BluetoothLeScanner _bleScanner;
+		private BleScanCallback _scanCallbackNew;
+		private LeScanCallback _scanCallbackOld;
 
+		private readonly BuildVersionCodes OS_VER;
 
-        public bool BluetoothIsEnableOnThisDevice()
-        {
-            if (!BluetoothIsAvailableOnThisDevice())
-            {
-                return false;
-            }
-
-            return _btAdapter.IsEnabled;
-        }
+		#endregion
 
 
-        public void RequestUserToTurnOnBluetooth()
-        {
-            if (!BluetoothIsAvailableOnThisDevice())
-            {
-                throw new BluetoothUnsupportedException("This device does not support Bluetooth.");
-            }
+		#region CONSTRUCTOR
 
-            Intent btTurnOnIntent = new Intent(BluetoothAdapter.ActionRequestEnable);
-            (Forms.Context as Activity).StartActivity(btTurnOnIntent);
-        }
+		public BeahatImplementation()
+		{
+			OS_VER = Build.VERSION.SdkInt;
 
+			_btManager = (BluetoothManager)Android.App.Application.Context.GetSystemService("bluetooth");
+			_btAdapter = _btManager.Adapter;
 
-        public void AddEvent(Guid uuid, ushort major, ushort minor, short thresholdRssi, int intervalMilliSec, Action function)
-        {
-            iBeaconEventHolder eventHolder = new iBeaconEventHolder(uuid, major, minor);
+			if (OS_VER < BuildVersionCodes.Lollipop)
+			{
+				// 4.4以下はこちら
+				_scanCallbackOld = new LeScanCallback();
+			}
+			else
+			{
+				// 5.0以上はこちら
+				_scanCallbackNew = new BleScanCallback();
+				try
+				{
+					// [エラー回避] 端末がBluetoothをサポートしていない場合、_btAdapterはnullになる。
+					_bleScanner = _btAdapter.BluetoothLeScanner;
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+				}
+			}
+		}
 
-            if (!_scanCallback.BeaconEventHolderDict.ContainsKey(eventHolder.BeaconIdentifyStr))
-            {
-                _scanCallback.BeaconEventHolderDict.Add(eventHolder.BeaconIdentifyStr, eventHolder);
-            }
-            _scanCallback.BeaconEventHolderDict[eventHolder.BeaconIdentifyStr].AddEvent(thresholdRssi, intervalMilliSec, function);
-        }
-
-
-        public void AddEvent(Guid uuid, ushort major, ushort minor)
-        {
-            iBeaconEventHolder eventHolder = new iBeaconEventHolder(uuid, major, minor);
-
-            if (!_scanCallback.BeaconEventHolderDict.ContainsKey(eventHolder.BeaconIdentifyStr))
-            {
-                _scanCallback.BeaconEventHolderDict.Add(eventHolder.BeaconIdentifyStr, eventHolder);
-            }
-        }
+		#endregion
 
 
-        public void ClearAllEvent()
-        {
-            _scanCallback.BeaconEventHolderDict = new Dictionary<string, iBeaconEventHolder>();
-            _scanCallback.DetectedBeaconDict = new Dictionary<string, iBeacon>();
-        }
+		#region PUBLIC METHODS
+
+		public bool BluetoothIsAvailableOnThisDevice()
+		{
+			return !(_btAdapter == null);
+		}
 
 
-        public void StartScan()
-        {
-            if (!BluetoothIsAvailableOnThisDevice())
-            {
-                throw new BluetoothUnsupportedException("This device does not support Bluetooth.");
-            }
+		public bool BluetoothIsEnableOnThisDevice()
+		{
+			if (!BluetoothIsAvailableOnThisDevice())
+			{
+				return false;
+			}
 
-            if (!BluetoothIsEnableOnThisDevice())
-            {
-                throw new BluetoothTurnedOffException("Bluetooth service on this device is turned off.");
-            }
-
-            if (IsScanning)
-            {
-                return;
-            }
-
-            if (_bleScanner == null)
-            {
-                _bleScanner = _btAdapter.BluetoothLeScanner;
-            }
-
-            _scanCallback.DetectedBeaconDict = new Dictionary<string, iBeacon>();
-            _bleScanner.StartScan(_scanCallback);
-            IsScanning = true;
-        }
+			return _btAdapter.IsEnabled;
+		}
 
 
-        public void StopScan()
-        {
-            if (!IsScanning)
-            {
-                return;
-            }
+		public void RequestUserToTurnOnBluetooth()
+		{
+			if (!BluetoothIsAvailableOnThisDevice())
+			{
+				throw new BluetoothUnsupportedException("This device does not support Bluetooth.");
+			}
 
-            _bleScanner.StopScan(_scanCallback);
-            IsScanning = false;
-        }
-
-        #endregion
-
-    }
+			Intent btTurnOnIntent = new Intent(BluetoothAdapter.ActionRequestEnable);
+			(Forms.Context as Activity).StartActivity(btTurnOnIntent);
+		}
 
 
-    class BleScanCallback : ScanCallback
-    {
+		public void AddEvent(Guid uuid, ushort major, ushort minor, short thresholdRssi, int intervalMilliSec, Action function)
+		{
+			iBeaconEventHolder eventHolder = new iBeaconEventHolder(uuid, major, minor);
 
-        #region PROPERTIES
+			if (OS_VER < BuildVersionCodes.Lollipop)
+			{
+				if (!_scanCallbackOld.BeaconEventHolderDict.ContainsKey(eventHolder.BeaconIdentifyStr))
+				{
+					_scanCallbackOld.BeaconEventHolderDict.Add(eventHolder.BeaconIdentifyStr, eventHolder);
+				}
+				_scanCallbackOld.BeaconEventHolderDict[eventHolder.BeaconIdentifyStr].AddEvent(thresholdRssi, intervalMilliSec, function);
+			}
+			else
+			{
+				if (!_scanCallbackNew.BeaconEventHolderDict.ContainsKey(eventHolder.BeaconIdentifyStr))
+				{
+					_scanCallbackNew.BeaconEventHolderDict.Add(eventHolder.BeaconIdentifyStr, eventHolder);
+				}
+				_scanCallbackNew.BeaconEventHolderDict[eventHolder.BeaconIdentifyStr].AddEvent(thresholdRssi, intervalMilliSec, function);
+			}
+		}
 
-        public Dictionary<string, iBeaconEventHolder> BeaconEventHolderDict { get; set; }
-        public Dictionary<string, iBeacon> DetectedBeaconDict { get; set; }
 
-        #endregion
+		public void AddEvent(Guid uuid, ushort major, ushort minor)
+		{
+			iBeaconEventHolder eventHolder = new iBeaconEventHolder(uuid, major, minor);
+
+			if (OS_VER < BuildVersionCodes.Lollipop)
+			{
+				if (!_scanCallbackOld.BeaconEventHolderDict.ContainsKey(eventHolder.BeaconIdentifyStr))
+				{
+					_scanCallbackOld.BeaconEventHolderDict.Add(eventHolder.BeaconIdentifyStr, eventHolder);
+				}
+			}
+			else
+			{
+				if (!_scanCallbackNew.BeaconEventHolderDict.ContainsKey(eventHolder.BeaconIdentifyStr))
+				{
+					_scanCallbackNew.BeaconEventHolderDict.Add(eventHolder.BeaconIdentifyStr, eventHolder);
+				}
+			}
+		}
 
 
-        #region CONSTRUCTOR
+		public void ClearAllEvent()
+		{
+			if (OS_VER < BuildVersionCodes.Lollipop)
+			{
+				_scanCallbackOld.BeaconEventHolderDict = new Dictionary<string, iBeaconEventHolder>();
+				_scanCallbackOld.DetectedBeaconDict = new Dictionary<string, iBeacon>();
+			}
+			else
+			{
+				_scanCallbackNew.BeaconEventHolderDict = new Dictionary<string, iBeaconEventHolder>();
+				_scanCallbackNew.DetectedBeaconDict = new Dictionary<string, iBeacon>();
+			}
+		}
 
-        public BleScanCallback()
-        {
-            BeaconEventHolderDict = new Dictionary<string, iBeaconEventHolder>();
-            DetectedBeaconDict = new Dictionary<string, iBeacon>();
-        }
 
-        #endregion
+		public void StartScan()
+		{
+			if (!BluetoothIsAvailableOnThisDevice())
+			{
+				throw new BluetoothUnsupportedException("This device does not support Bluetooth.");
+			}
+
+			if (!BluetoothIsEnableOnThisDevice())
+			{
+				throw new BluetoothTurnedOffException("Bluetooth service on this device is turned off.");
+			}
+
+			if (IsScanning)
+			{
+				return;
+			}
+
+			if (OS_VER < BuildVersionCodes.Lollipop)
+			{
+				_scanCallbackOld.DetectedBeaconDict = new Dictionary<string, iBeacon>();
+				_btAdapter.StartLeScan(_scanCallbackOld);
+			}
+			else
+			{
+				if (_bleScanner == null)
+				{
+					_bleScanner = _btAdapter.BluetoothLeScanner;
+				}
+				_scanCallbackNew.DetectedBeaconDict = new Dictionary<string, iBeacon>();
+				_bleScanner.StartScan(_scanCallbackNew);
+			}
+			IsScanning = true;
+		}
 
 
-        #region OVERRIDE METHODS
+		public void StopScan()
+		{
+			if (!IsScanning)
+			{
+				return;
+			}
 
-        public override void OnScanResult(ScanCallbackType callbackType, ScanResult result)
-        {
-            base.OnScanResult(callbackType, result);
+			if (OS_VER < BuildVersionCodes.Lollipop)
+			{
+				_btAdapter.StopLeScan(_scanCallbackOld);
+			}
+			else
+			{
+				_bleScanner.StopScan(_scanCallbackNew);
+			}
 
-            if (iBeaconDroidUtility.IsIBeacon(result.ScanRecord))
-            {
-                Guid uuid = iBeaconDroidUtility.GetUuidFromRecord(result.ScanRecord);
-                ushort major = iBeaconDroidUtility.GetMajorFromRecord(result.ScanRecord);
-                ushort minor = iBeaconDroidUtility.GetMinorFromRecord(result.ScanRecord);
+			IsScanning = false;
+		}
 
-                string beaconIdentifier = iBeaconEventHolder.GenerateBeaconIdentifyStr(uuid, major, minor);
+		#endregion
 
-                if (!BeaconEventHolderDict.ContainsKey(beaconIdentifier))
-                {
-                    return;
-                }
+	}
 
-                iBeaconEventHolder eventHolder = BeaconEventHolderDict[beaconIdentifier];
-                iBeacon holdBeacon = eventHolder.ibeacon;
 
-                if (DetectedBeaconDict.ContainsKey(beaconIdentifier))
-                {
-                    iBeacon detectedBeaconPrev = DetectedBeaconDict[beaconIdentifier];
-                    short? rssiPrev = detectedBeaconPrev.Rssi;
+	#region CALLBACK FOR ANDROID VER 4.4
 
-                    if (rssiPrev == null || ((short)rssiPrev < result.Rssi))
-                    {
-                        holdBeacon.Rssi = (short)result.Rssi;
-                        holdBeacon.TxPower = iBeaconDroidUtility.GetTxPowerFromRecord(result.ScanRecord);
-                        holdBeacon.EstimatedDistanceMeter = iBeaconDroidUtility.CalcDistanceMeterFromRssiAndTxPower(holdBeacon.Rssi, holdBeacon.TxPower);
-                        DetectedBeaconDict[beaconIdentifier] = holdBeacon;
-                    }
-                }
-                else
-                {
-                    holdBeacon.Rssi = (short)result.Rssi;
-                    holdBeacon.TxPower = iBeaconDroidUtility.GetTxPowerFromRecord(result.ScanRecord);
-                    holdBeacon.EstimatedDistanceMeter = iBeaconDroidUtility.CalcDistanceMeterFromRssiAndTxPower(holdBeacon.Rssi, holdBeacon.TxPower);
-                    DetectedBeaconDict.Add(beaconIdentifier, holdBeacon);
-                }
+	class LeScanCallback : Java.Lang.Object, BluetoothAdapter.ILeScanCallback
+	{
 
-                foreach (var eventDetail in eventHolder.EventList)
-                {
-                    if (eventDetail.ThresholdRssi < result.Rssi &&
-                        eventDetail.LastTriggeredDateTime < DateTime.Now.AddMilliseconds(-1 * eventDetail.EventTriggerIntervalMilliSec))
-                    {
-                        eventDetail.LastTriggeredDateTime = DateTime.Now;
-                        eventDetail.Function();
-                    }
-                }
-            }
-        }
+		#region PROPERTIES
 
-        #endregion
+		public Dictionary<string, iBeaconEventHolder> BeaconEventHolderDict { get; set; }
+		public Dictionary<string, iBeacon> DetectedBeaconDict { get; set; }
 
-    }
+		#endregion
+
+
+		#region CONSTRUCTOR
+
+		public LeScanCallback()
+		{
+			BeaconEventHolderDict = new Dictionary<string, iBeaconEventHolder>();
+			DetectedBeaconDict = new Dictionary<string, iBeacon>();
+		}
+
+		#endregion
+
+
+		public void OnLeScan(BluetoothDevice device, int rssi, byte[] scanRecord)
+		{
+			if (iBeaconDroidUtility.IsIBeacon(scanRecord))
+			{
+				Guid uuid = iBeaconDroidUtility.GetUuidFromRecord(scanRecord);
+				ushort major = iBeaconDroidUtility.GetMajorFromRecord(scanRecord);
+				ushort minor = iBeaconDroidUtility.GetMinorFromRecord(scanRecord);
+
+				string beaconIdentifier = iBeaconEventHolder.GenerateBeaconIdentifyStr(uuid, major, minor);
+
+				if (!BeaconEventHolderDict.ContainsKey(beaconIdentifier))
+				{
+					return;
+				}
+
+				iBeaconEventHolder eventHolder = BeaconEventHolderDict[beaconIdentifier];
+
+				if (DetectedBeaconDict.ContainsKey(beaconIdentifier))
+				{
+					iBeacon detectedBeaconPrev = DetectedBeaconDict[beaconIdentifier];
+					short? rssiPrev = detectedBeaconPrev.Rssi;
+
+					if (rssiPrev == null || ((short)rssiPrev < rssi))
+					{
+						eventHolder.ibeacon.Rssi = (short)rssi;
+						DetectedBeaconDict[beaconIdentifier] = eventHolder.ibeacon;
+					}
+				}
+				else
+				{
+					eventHolder.ibeacon.Rssi = (short)rssi;
+					eventHolder.ibeacon.TxPower = iBeaconDroidUtility.GetTxPowerFromRecord(scanRecord);
+					DetectedBeaconDict.Add(beaconIdentifier, eventHolder.ibeacon);
+				}
+
+				foreach (var eventDetail in eventHolder.EventList)
+				{
+					if (eventDetail.ThresholdRssi < rssi &&
+						eventDetail.LastTriggeredDateTime < DateTime.Now.AddMilliseconds(-1 * eventDetail.EventTriggerIntervalMilliSec))
+					{
+						eventDetail.LastTriggeredDateTime = DateTime.Now;
+						eventDetail.Function();
+					}
+				}
+			}
+		}
+	}
+
+	#endregion
+
+
+	#region CALLBACK FOR ANDROID VER 5.0 AND OVER
+
+	class BleScanCallback : ScanCallback
+	{
+
+		#region PROPERTIES
+
+		public Dictionary<string, iBeaconEventHolder> BeaconEventHolderDict { get; set; }
+		public Dictionary<string, iBeacon> DetectedBeaconDict { get; set; }
+
+		#endregion
+
+
+		#region CONSTRUCTOR
+
+		public BleScanCallback()
+		{
+			BeaconEventHolderDict = new Dictionary<string, iBeaconEventHolder>();
+			DetectedBeaconDict = new Dictionary<string, iBeacon>();
+		}
+
+		#endregion
+
+
+		#region OVERRIDE METHODS
+
+		public override void OnScanResult(ScanCallbackType callbackType, ScanResult result)
+		{
+			base.OnScanResult(callbackType, result);
+
+			if (iBeaconDroidUtility.IsIBeacon(result.ScanRecord))
+			{
+				Guid uuid = iBeaconDroidUtility.GetUuidFromRecord(result.ScanRecord);
+				ushort major = iBeaconDroidUtility.GetMajorFromRecord(result.ScanRecord);
+				ushort minor = iBeaconDroidUtility.GetMinorFromRecord(result.ScanRecord);
+
+				string beaconIdentifier = iBeaconEventHolder.GenerateBeaconIdentifyStr(uuid, major, minor);
+
+				if (!BeaconEventHolderDict.ContainsKey(beaconIdentifier))
+				{
+					return;
+				}
+
+				iBeaconEventHolder eventHolder = BeaconEventHolderDict[beaconIdentifier];
+				iBeacon holdBeacon = eventHolder.ibeacon;
+
+				if (DetectedBeaconDict.ContainsKey(beaconIdentifier))
+				{
+					iBeacon detectedBeaconPrev = DetectedBeaconDict[beaconIdentifier];
+					short? rssiPrev = detectedBeaconPrev.Rssi;
+
+					if (rssiPrev == null || ((short)rssiPrev < result.Rssi))
+					{
+						holdBeacon.Rssi = (short)result.Rssi;
+						holdBeacon.TxPower = iBeaconDroidUtility.GetTxPowerFromRecord(result.ScanRecord);
+						holdBeacon.EstimatedDistanceMeter = iBeaconDroidUtility.CalcDistanceMeterFromRssiAndTxPower(holdBeacon.Rssi, holdBeacon.TxPower);
+						DetectedBeaconDict[beaconIdentifier] = holdBeacon;
+					}
+				}
+				else
+				{
+					holdBeacon.Rssi = (short)result.Rssi;
+					holdBeacon.TxPower = iBeaconDroidUtility.GetTxPowerFromRecord(result.ScanRecord);
+					holdBeacon.EstimatedDistanceMeter = iBeaconDroidUtility.CalcDistanceMeterFromRssiAndTxPower(holdBeacon.Rssi, holdBeacon.TxPower);
+					DetectedBeaconDict.Add(beaconIdentifier, holdBeacon);
+				}
+
+				foreach (var eventDetail in eventHolder.EventList)
+				{
+					if (eventDetail.ThresholdRssi < result.Rssi &&
+						eventDetail.LastTriggeredDateTime < DateTime.Now.AddMilliseconds(-1 * eventDetail.EventTriggerIntervalMilliSec))
+					{
+						eventDetail.LastTriggeredDateTime = DateTime.Now;
+						eventDetail.Function();
+					}
+				}
+			}
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
 }
--- Beahat/Plugin.Beahat.Android/iBeaconDroidUtility.cs
+++ Beahat/Plugin.Beahat.Android/iBeaconDroidUtility.cs
@@ -21,6 +21,21 @@ namespace Plugin.Beahat
         }
 
 
+		public static bool IsIBeacon(byte[] scanRecord)
+		{
+			if (scanRecord.Length > 30 &&
+				scanRecord[5] == 0x4c &&
+				scanRecord[6] == 0x00 &&
+				scanRecord[7] == 0x02 &&
+				scanRecord[8] == 0x15)
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+
         public static Guid GetUuidFromRecord(ScanRecord scanRecord)
         {
             byte[] recordByteData = scanRecord.GetBytes();
@@ -34,6 +49,18 @@ namespace Plugin.Beahat
         }
 
 
+        public static Guid GetUuidFromRecord(byte[] scanRecord)
+		{
+			string uuidStr = BitConverter.ToString(scanRecord, 9, 4).Replace("-", "") + "-" +
+							 BitConverter.ToString(scanRecord, 13, 2).Replace("-", "") + "-" +
+							 BitConverter.ToString(scanRecord, 15, 2).Replace("-", "") + "-" +
+							 BitConverter.ToString(scanRecord, 17, 2).Replace("-", "") + "-" +
+							 BitConverter.ToString(scanRecord, 19, 6).Replace("-", "");
+
+			return new Guid(uuidStr);
+        }
+
+
         public static ushort GetMajorFromRecord(ScanRecord scanRecord)
         {
             byte[] recordByteData = scanRecord.GetBytes();
@@ -41,6 +68,15 @@ namespace Plugin.Beahat
             ushort major = Convert.ToUInt16(majorStr, 16);
 
             return major;
+        }
+
+
+        public static ushort GetMajorFromRecord(byte[] scanRecord)
+		{
+			string majorStr = BitConverter.ToString(scanRecord, 25, 2).Replace("-", "");
+			ushort major = Convert.ToUInt16(majorStr, 16);
+
+			return major;
         }
 
 
@@ -54,6 +90,15 @@ namespace Plugin.Beahat
         }
 
 
+        public static ushort GetMinorFromRecord(byte[] scanRecord)
+		{
+			string minorStr = BitConverter.ToString(scanRecord, 27, 2).Replace("-", "");
+			ushort minor = Convert.ToUInt16(minorStr, 16);
+
+			return minor;
+        }
+
+
         public static short GetTxPowerFromRecord(ScanRecord scanRecord)
         {
             byte[] recordByteData = scanRecord.GetBytes();
@@ -62,6 +107,16 @@ namespace Plugin.Beahat
 
             return txPower;
         }
+
+
+		public static short GetTxPowerFromRecord(byte[] scanRecord)
+		{
+			string txPowerStr = BitConverter.ToString(scanRecord, 29, 1);
+			short txPower = Convert.ToSByte(txPowerStr, 16);
+
+			return txPower;
+		}
+
 
         public static double? CalcDistanceMeterFromRssiAndTxPower(short? rssi, short? txPower)
         {
